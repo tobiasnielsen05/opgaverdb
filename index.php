f5e6e15cd8aec366805177450985725eefb55b87
Tobias <ton001@edu.zealand.dk>
2024-10-08 11:52:57 +0200

    Ændret title på index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require "settings/init.php";
 <head>
     <meta charset="utf-8">
 
-    <title>Opgaver</title>
+    <title>Forside</title>
 
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
